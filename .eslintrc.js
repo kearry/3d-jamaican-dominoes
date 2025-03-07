28995f2dc123f3a50a270a1b6ccfebf3eed37087
kearry <oracle.professional@yahoo.co.uk>
2025-03-07 18:02:34 +0000

    Add comprehensive ESLint configuration

---
+++ .eslintrc.js
@@ -0,0 +1,97 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jsx-a11y/recommended',
+    'next/core-web-vitals',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'jsx-a11y',
+    'react-hooks',
+    'prettier',
+  ],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  rules: {
+    // React specific rules
+    'react/prop-types': 'off', // We use TypeScript for prop validation
+    'react/react-in-jsx-scope': 'off', // Not needed with Next.js
+    'react/display-name': 'off',
+    'react/jsx-filename-extension': [
+      1,
+      { extensions: ['.tsx', '.jsx'] }
+    ],
+    
+    // React hooks rules
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    
+    // TypeScript specific rules
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/ban-ts-comment': [
+      'error',
+      {
+        'ts-ignore': 'allow-with-description',
+        'ts-expect-error': 'allow-with-description',
+      },
+    ],
+    
+    // Accessibility rules
+    'jsx-a11y/anchor-is-valid': [
+      'error',
+      {
+        components: ['Link'],
+        specialLink: ['hrefLeft', 'hrefRight'],
+        aspects: ['invalidHref', 'preferButton'],
+      },
+    ],
+    
+    // General code quality rules
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'eqeqeq': ['error', 'always'],
+    'curly': ['error', 'all'],
+    'prefer-const': 'error',
+    'no-var': 'error',
+    'no-duplicate-imports': 'error',
+    
+    // Prettier
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+  },
+  overrides: [
+    {
+      // Test files
+      files: ['**/*.test.tsx', '**/*.test.ts', '**/*.spec.tsx', '**/*.spec.ts'],
+      env: {
+        jest: true,
+      },
+      rules: {
+        '@typescript-eslint/no-explicit-any': 'off',
+        'no-console': 'off',
+      },
+    },
+  ],
+};
